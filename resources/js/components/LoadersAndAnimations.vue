b26fe2c1794da8791d032be3cebe2369aff02619
AndySong <andylauszp@gmail.com>
2020-02-20 11:39:35 +1300

    wip

--- resources/js/components/LoadersAndAnimations.vue
+++ resources/js/components/LoadersAndAnimations.vue
@@ -1,8 +1,18 @@
 <template>
     <div>
-        <h1 class="text-4xl font-normal text-gray-900 leading-none">
+        <h1 class="text-4xl font-normal text-gray-900 leading-none mb-8">
             Loaders and Animations
         </h1>
+
+        <p class="mb-8">
+            These are implemented with Lottie, to make them work in your code
+            you have to…
+        </p>
+
+        <div
+            id="lottie"
+            class="shadow rounded-lg flex items-center justify-center mb-6"
+        ></div>
     </div>
 </template>
 
--- resources/js/components/Typography.vue
+++ resources/js/components/Typography.vue
@@ -1,8 +1,72 @@
 <template>
     <div>
-        <h1 class="text-4xl font-normal text-gray-900 leading-none">
-            Typography
+        <h1 class="text-4xl font-normal text-gray-900 leading-none mb-8">
+            Open Sans
         </h1>
+
+        <div
+            class="rounded-lg bg-gray-100 py-12 px-16 mb-10"
+            style="max-width: 522px"
+        >
+            <div class="flex justify-end mb-10 items-center">
+                <div class="rounded-full bg-black shadow w-8 h-8 mr-3"></div>
+                <span class="text-xs">#222222</span>
+            </div>
+
+            <div class="flex justify-between mb-6">
+                <strong class="text-xl font-light">Open Sans Light</strong>
+                <span class="text-xl font-light">300</span>
+            </div>
+
+            <div class="flex justify-between mb-6">
+                <strong class="text-xl font-normal">Open Sans Regular</strong>
+                <span class="text-xl font-normal">400</span>
+            </div>
+
+            <div class="flex justify-between mb-6">
+                <strong class="text-xl font-semibold"
+                    >Open Sans Semi Bold</strong
+                >
+                <span class="text-xl font-semibold">600</span>
+            </div>
+
+            <div class="flex justify-between">
+                <strong class="text-xl font-bold">Open Sans Bold</strong>
+                <span class="text-xl font-bold">700</span>
+            </div>
+        </div>
+
+        <div
+            class="rounded-lg bg-black text-white py-12 px-16 mb-8"
+            style="max-width: 522px"
+        >
+            <div class="flex justify-end mb-10 items-center">
+                <div class="rounded-full bg-white shadow w-8 h-8 mr-3"></div>
+                <span class="text-xs">#FFFFFF</span>
+            </div>
+
+            <div class="flex justify-between mb-6">
+                <strong class="text-xl font-light">Open Sans Light</strong>
+                <span class="text-xl font-light">300</span>
+            </div>
+
+            <div class="flex justify-between mb-6">
+                <strong class="text-xl font-normal">Open Sans Regular</strong>
+                <span class="text-xl font-normal">400</span>
+            </div>
+
+            <div class="flex justify-between mb-6">
+                <strong class="text-xl font-semibold"
+                    >Open Sans Semi Bold</strong
+                >
+                <span class="text-xl font-semibold">600</span>
+            </div>
+
+            <div class="flex justify-between">
+                <strong class="text-xl font-bold">Open Sans Bold</strong>
+                <span class="text-xl font-bold">700</span>
+            </div>
+        </div>
     </div>
 </template>
 
