e27b95c255c72600e814d8417783b18dabe9dc86
AndySong <andylauszp@gmail.com>
2020-02-20 11:49:42 +1300

    wip

--- resources/js/components/LoadersAndAnimations.vue
+++ resources/js/components/LoadersAndAnimations.vue
@@ -4,22 +4,40 @@
             Loaders and Animations
         </h1>
 
-        <p class="mb-8">
-            These are implemented with Lottie, to make them work in your code
-            you have to…
+        <p class="mb-10 text-grey-900 leading-normal">
+            This spinner uses AirBnb's
+            <a
+                href="http://airbnb.io/lottie/#/web"
+                class="text-as-blue font-bold"
+                >Lottie</a
+            >
+            library to render its animation. You may download the necessary
+            assets below.
         </p>
-
         <div
-            id="lottie"
-            class="shadow rounded-lg flex items-center justify-center mb-6"
-        ></div>
+            class="border border-grey-200 rounded-lg flex items-center justify-center mb-8"
+        >
+            <div ref="lottie" class="w-64" style="height: 457px"></div>
+        </div>
+        <a
+            href="/images/logo.svg"
+            class="border px-10 py-2 rounded-full text-black font-bold text-xs"
+            >spinner.zip
+        </a>
     </div>
 </template>
 
 <script>
+import lottie from 'lottie-web'
 export default {
     mounted() {
-        console.log('About Component mounted.')
+        lottie.loadAnimation({
+            container: this.$refs.lottie,
+            renderer: 'svg',
+            loop: true,
+            autoplay: true,
+            path: '/animations/spinner.json'
+        })
     }
 }
 </script>
